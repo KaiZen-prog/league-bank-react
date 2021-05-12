0b54adcf691931b1020f8b006ee71fd1d1d28c37
Николай Карнаев <karnaev.nick@gmail.com>
2021-05-13 00:09:22 +0300

    Хедер Мобильный WIP

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,34 @@
 import React from "react";
+import withHeader from "../../hocs/with-header";
 
-const Header = () => {
+const Header = (props) => {
+
+    const {
+        isNavOpened,
+        onNavOpen,
+        onNavClose
+    } = props;
+
     return (
         <header className="header">
             <div className="header__wrapper">
+                <button
+                    className="header__burger-button"
+                    onClick={onNavOpen}
+                >
+                    <span className="visually-hidden">Открыть навигацию</span>
+                </button>
+
                 <a className="header__logo" href="#">ЛИГА Банк</a>
 
-                <nav className="header__nav">
+                <button
+                    className={`header__close-button ${isNavOpened ? `header__close-button--opened` : ``}`}
+                    onClick={onNavClose}
+                >
+                    <span className="visually-hidden">Закрыть навигацию</span>
+                </button>
+
+                <nav className={`header__nav ${isNavOpened ? `header__nav--opened` : ``}`}>
                     <ul className="header__nav-list">
                         <li className="header__nav-item">
                             <a className="header__nav-link" href="#">Услуги</a>
@@ -33,4 +55,4 @@ const Header = () => {
 
 Header.displayName = `Header`;
 
-export default Header;
+export default withHeader(Header);
