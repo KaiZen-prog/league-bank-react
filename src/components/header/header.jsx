bdb1c5fb2312c193dd4d7a48fd0b44589665df86
Николай Карнаев <karnaev.nick@gmail.com>
2021-05-13 08:55:35 +0300

    Хедер WIP

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -28,26 +28,28 @@ const Header = (props) => {
                     <span className="visually-hidden">Закрыть навигацию</span>
                 </button>
 
-                <nav className={`header__nav ${isNavOpened ? `header__nav--opened` : ``}`}>
-                    <ul className="header__nav-list">
-                        <li className="header__nav-item">
-                            <a className="header__nav-link" href="#">Услуги</a>
-                        </li>
-                        <li className="header__nav-item">
-                            <a className="header__nav-link" href="#">Рассчитать кредит</a>
-                        </li>
-                        <li className="header__nav-item">
-                            <a className="header__nav-link" href="#">Конвертер валют</a>
-                        </li>
-                        <li className="header__nav-item">
-                            <a className="header__nav-link" href="#">Контакты</a>
-                        </li>
-                    </ul>
-                </nav>
+                <div className="header__nav-wrapper">
+                    <nav className={`header__nav ${isNavOpened ? `header__nav--opened` : ``}`}>
+                        <ul className="header__nav-list">
+                            <li className="header__nav-item">
+                                <a className="header__nav-link" href="#">Услуги</a>
+                            </li>
+                            <li className="header__nav-item">
+                                <a className="header__nav-link" href="#">Рассчитать кредит</a>
+                            </li>
+                            <li className="header__nav-item">
+                                <a className="header__nav-link" href="#">Конвертер валют</a>
+                            </li>
+                            <li className="header__nav-item">
+                                <a className="header__nav-link" href="#">Контакты</a>
+                            </li>
+                        </ul>
+                    </nav>
 
-                <a className="header__login" href="#">
-                    <span className="header__login-title">Войти в Интернет-банк</span>
-                </a>
+                    <a className="header__login" href="#">
+                        <span className="header__login-title">Войти в Интернет-банк</span>
+                    </a>
+                </div>
             </div>
         </header>
     );
