af8b25e5fdb9d7a52f20b1e7b4346961770e369d
Николай Карнаев <karnaev.nick@gmail.com>
2021-05-13 21:08:12 +0300

    Хедер WIP

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,58 +1,67 @@
 import React from "react";
+import PropTypes from "prop-types";
 import withHeader from "../../hocs/with-header";
 
 const Header = (props) => {
+  const {
+    isNavOpened,
+    onNavOpen,
+    onNavClose
+  } = props;
 
-    const {
-        isNavOpened,
-        onNavOpen,
-        onNavClose
-    } = props;
+  return (
+    <header className="header">
+      <div className="header__wrapper">
+        <button
+          className="header__burger-button"
+          onClick={onNavOpen}
+        >
+          <span className="visually-hidden">Открыть навигацию</span>
+        </button>
 
-    return (
-        <header className="header">
-            <div className="header__wrapper">
-                <button
-                    className="header__burger-button"
-                    onClick={onNavOpen}
-                >
-                    <span className="visually-hidden">Открыть навигацию</span>
-                </button>
+        <a className="header__logo" href="#">ЛИГА Банк</a>
 
-                <a className="header__logo" href="#">ЛИГА Банк</a>
+        <button
+          className={`header__close-button ${isNavOpened ? `header__close-button--opened` : ``}`}
+          onClick={onNavClose}
+        >
+          <span className="visually-hidden">Закрыть навигацию</span>
+        </button>
 
-                <button
-                    className={`header__close-button ${isNavOpened ? `header__close-button--opened` : ``}`}
-                    onClick={onNavClose}
-                >
-                    <span className="visually-hidden">Закрыть навигацию</span>
-                </button>
+        <div className="header__nav-wrapper">
+          <nav className={`header__nav ${isNavOpened ? `header__nav--opened` : ``}`}>
+            <ul className="header__nav-list">
+              <li className="header__nav-item">
+                <a className="header__nav-link" href="#">Услуги</a>
+              </li>
 
-                <div className="header__nav-wrapper">
-                    <nav className={`header__nav ${isNavOpened ? `header__nav--opened` : ``}`}>
-                        <ul className="header__nav-list">
-                            <li className="header__nav-item">
-                                <a className="header__nav-link" href="#">Услуги</a>
-                            </li>
-                            <li className="header__nav-item">
-                                <a className="header__nav-link" href="#">Рассчитать кредит</a>
-                            </li>
-                            <li className="header__nav-item">
-                                <a className="header__nav-link" href="#">Конвертер валют</a>
-                            </li>
-                            <li className="header__nav-item">
-                                <a className="header__nav-link" href="#">Контакты</a>
-                            </li>
-                        </ul>
-                    </nav>
+              <li className="header__nav-item">
+                <a className="header__nav-link" href="#">Рассчитать кредит</a>
+              </li>
 
-                    <a className="header__login" href="#">
-                        <span className="header__login-title">Войти в Интернет-банк</span>
-                    </a>
-                </div>
-            </div>
-        </header>
-    );
+              <li className="header__nav-item">
+                <a className="header__nav-link" href="#">Конвертер валют</a>
+              </li>
+
+              <li className="header__nav-item">
+                <a className="header__nav-link" href="#">Контакты</a>
+              </li>
+            </ul>
+          </nav>
+
+          <a className={`header__login ${isNavOpened ? `header__login-opened` : ``}`} href="#">
+            <span className="header__login-title">Войти в Интернет-банк</span>
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+Header.propTypes = {
+  isNavOpened: PropTypes.bool.isRequired,
+  onNavOpen: PropTypes.func.isRequired,
+  onNavClose: PropTypes.func.isRequired
 };
 
 Header.displayName = `Header`;
--- src/hocs/with-header.jsx
+++ src/hocs/with-header.jsx
@@ -1,38 +1,38 @@
 import React from 'react';
 
 const withHeader = (Component) => {
-    class WithHeader extends React.PureComponent {
-        constructor(props) {
-            super(props);
+  class WithHeader extends React.PureComponent {
+    constructor(props) {
+      super(props);
 
-            this.state = {
-                isNavOpened: false,
-            }
+      this.state = {
+        isNavOpened: false,
+      };
 
-            this.onNavOpen = this.onNavOpen.bind(this);
-            this.onNavClose = this.onNavClose.bind(this);
-        }
-
-        onNavOpen() {
-            this.setState({isNavOpened: true});
-        }
-
-        onNavClose() {
-            this.setState({isNavOpened: false});
-        }
-
-        render() {
-            return (
-                <Component
-                    isNavOpened={this.state.isNavOpened}
-                    onNavOpen={this.onNavOpen}
-                    onNavClose={this.onNavClose}
-                />
-            )
-        }
+      this.onNavOpen = this.onNavOpen.bind(this);
+      this.onNavClose = this.onNavClose.bind(this);
     }
 
-    return WithHeader;
+    onNavOpen() {
+      this.setState({isNavOpened: true});
+    }
+
+    onNavClose() {
+      this.setState({isNavOpened: false});
+    }
+
+    render() {
+      return (
+        <Component
+          isNavOpened={this.state.isNavOpened}
+          onNavOpen={this.onNavOpen}
+          onNavClose={this.onNavClose}
+        />
+      );
+    }
+  }
+
+  return WithHeader;
 };
 
 export default withHeader;
