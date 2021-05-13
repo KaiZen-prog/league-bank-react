6d0beb39182b2ba8f2db8c111a86db45c7160218
Николай Карнаев <karnaev.nick@gmail.com>
2021-05-13 23:29:58 +0300

    Слайдер WIP

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "../header/header";
+import MainSlider from "../main-slider/main-slider";
 
 const App = () => {
   return (
     <>
       <Header/>
+      <main>
+        <MainSlider/>
+      </main>
     </>
   );
 };
