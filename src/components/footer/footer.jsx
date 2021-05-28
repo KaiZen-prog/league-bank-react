29ddf613aa5edaa903f9eb905b2aa32ac104628a
Николай Карнаев <karnaev.nick@gmail.com>
2021-05-29 00:44:13 +0300

    Футер WIP

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,58 +5,61 @@ const Footer = () => {
   return (
       <footer className="footer">
         <div className="footer__wrapper">
-          <a className="footer__logo" href="#">ЛИГА Банк</a>
+          <div className="footer__container">
+            <a className="footer__logo" href="#">ЛИГА Банк</a>
 
-          <nav className="footer__nav">
-            <ul className="footer__nav-list">
-              <li className="footer__nav-item">
-                <a className="footer__nav-link" href="#">Услуги</a>
+            <nav className="footer__nav">
+              <ul className="footer__nav-list">
+                <li className="footer__nav-item">
+                  <a className="footer__nav-link" href="#">Услуги</a>
+                </li>
+
+                <li className="footer__nav-item">
+                  <a className="footer__nav-link" href="#">Рассчитать кредит</a>
+                </li>
+
+                <li className="footer__nav-item">
+                  <a className="footer__nav-link" href="#">Контакты</a>
+                </li>
+
+                <li className="footer__nav-item">
+                  <a className="footer__nav-link" href="#">Задать вопрос</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
+          <div className="footer__container">
+            <ul className="footer__phone-list">
+              <li className="footer__phone-item">
+                <a className="footer__phone-link footer__phone-link--mobile" href="tel:*0904">*0904</a>
+                <p className="footer__phone-description">Бесплатно для абонентов МТС, Билайн, Мегафон, Теле2</p>
               </li>
 
-              <li className="footer__nav-item">
-                <a className="footer__nav-link" href="#">Рассчитать кредит</a>
-              </li>
-
-              <li className="footer__nav-item">
-                <a className="footer__nav-link" href="#">Контакты</a>
-              </li>
-
-              <li className="footer__nav-item">
-                <a className="footer__nav-link" href="#">Задать вопрос</a>
+              <li className="footer__phone-item">
+                <a className="footer__phone-link footer__phone-link--phone" href="tel:88001112233">8 800 111 22 33</a>
+                <p className="footer__phone-description">Бесплатный для всех городов России</p>
               </li>
             </ul>
-          </nav>
 
-          <ul className="footer__phone-list">
-            <li className="footer__phone-item">
-              <a className="footer__phone-link footer__phone-link--mobile" href="tel:*0904">*0904</a>
-              <p className="footer__phone-description">Бесплатно для абонентов МТС, Билайн, Мегафон, Теле2</p>
-            </li>
+            <ul className="footer__social-list">
+              <li className="footer__social-item">
+                <a className="footer__social-link footer__social-link--facebook" href="#howhere" aria-label="Наш Фейсбук"></a>
+              </li>
 
-            <li className="footer__phone-item">
-              <a className="footer__phone-link footer__phone-link--phone" href="tel:88001112233">8 800 111 22 33</a>
-              <p className="footer__phone-description">Бесплатный для всех городов России</p>
-            </li>
-          </ul>
+              <li className="footer__social-item">
+                <a className="footer__social-link footer__social-link--instagram" href="#howhere" aria-label="Наш Инстаграм"></a>
+              </li>
 
-          <ul className="footer__social-list">
-            <li className="footer__social-item">
-              <a className="footer__social-link footer__social-link--facebook" href="#howhere" aria-label="Наш Фейсбук"></a>
-            </li>
+              <li className="footer__social-item">
+                <a className="footer__social-link footer__social-link--twitter" href="#howhere" aria-label="Наш Твиттер"></a>
+              </li>
 
-            <li className="footer__social-item">
-              <a className="footer__social-link footer__social-link--instagram" href="#howhere" aria-label="Наш Инстаграм"></a>
-            </li>
-
-            <li className="footer__social-item">
-              <a className="footer__social-link footer__social-link--twitter" href="#howhere" aria-label="Наш Твиттер"></a>
-            </li>
-
-            <li className="footer__social-item">
-              <a className="footer__social-link footer__social-link--youtube" href="#howhere" aria-label="Мы на Ютьюб"></a>
-            </li>
-          </ul>
-
+              <li className="footer__social-item">
+                <a className="footer__social-link footer__social-link--youtube" href="#howhere" aria-label="Мы на Ютьюб"></a>
+              </li>
+            </ul>
+          </div>
         </div>
       </footer>
   );
