1094d7c0dfcc829770d605c8ec871baf4450fa2e
KaiZen-prog <karnaev.nick@gmail.com>
2022-12-15 17:17:10 +0600

    Вносит незначительные правки

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {HashRouter} from 'react-router-dom';
 import thunk from 'redux-thunk';
-import { createAPI } from './services/api';
-import { createStore, applyMiddleware } from 'redux';
+import {createAPI} from './services/api';
+import {createStore, applyMiddleware} from 'redux';
 import rootReducer from './store/reducers/root-reducer';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import App from './components/app/app';
 
 const api = createAPI();
