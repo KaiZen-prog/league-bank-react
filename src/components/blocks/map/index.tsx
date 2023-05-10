380c8aa87c2a154cc553878c3a343842927c5e01
KaiZen-prog <karnaev.nick@gmail.com>
2023-05-10 13:43:40 +0300

    Корректирует блок с картой

--- src/components/blocks/map/index.tsx
+++ src/components/blocks/map/index.tsx
@@ -8,7 +8,7 @@ const Map: React.FunctionComponent = () => {
       <Iframe
         title="map"
         className="map__iframe"
-        src="?um=constructor%3A903677e276cd647a44c0897e60edad9ae34a0ebe73fde4d37f4ca90b4001c09f&amp;source=constructor"
+        src="https://yandex.ru/map-widget/v1/?um=constructor%3A903677e276cd647a44c0897e60edad9ae34a0ebe73fde4d37f4ca90b4001c09f&amp;source=constructor"
       >
       </Iframe>
     </MapBlock>
