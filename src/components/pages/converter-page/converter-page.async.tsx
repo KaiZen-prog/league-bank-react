8d5b2a0fb903ba3052e95c8579777d700101174c
KaiZen-prog <60105826+KaiZen-prog@users.noreply.github.com>
2023-12-07 16:16:14 +0300

    Убирает искусственные таймеры у ленивой загрузки страниц

--- src/components/pages/converter-page/converter-page.async.tsx
+++ src/components/pages/converter-page/converter-page.async.tsx
@@ -1,6 +1,3 @@
 import {lazy} from 'react';
 
-export const ConverterPageAsync = lazy(() => new Promise((resolve) => {
-  //@ts-ignore
-  setTimeout(() => resolve(import('./converter-page')), 3000);
-}));
+export const ConverterPageAsync = lazy(() => import('./converter-page'));
